2084bb45aa228e9ec612333b3102dbaad03c94e7
miraj004 <miraj.cei@gmail.com>
2024-12-04 21:24:35 +0430

    Read Me File Added

--- 01-starting-project/src/components/Tab/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-import './TabButton.css'
-
-export default function TabButton({active, handleClick, children}) {
-    return (
-        <li><button onClick={handleClick} className={active ? 'active':''}>{children}</button></li>
-    )
-}
--- 01-starting-project/src/components/Tab/TabContent.jsx
+++
@@ -1,9 +0,0 @@
-import './TabContent.css'
-export default function TabContent({concept}) {
-    return(
-        <div id="tab-content">
-             <h3>{concept.title}</h3>
-             <code>{concept.description}</code>
-        </div>
-    )
-}
